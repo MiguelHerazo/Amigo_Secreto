28a4289eabc530e835ca163233b9c98d3584b7f8
MiguelHerazo7 <herazomigue56@gmail.com>
2025-03-12 18:51:11 -0500

    Se agrega la funcion de sortear Amigo y se cambia el html para mostrar el texto

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let nombres = [];
 
 //funcion para crear amigos
 function agregarAmigo() {
-  let amigo = document.getElementById("amigo").value;
+  let amigo = document.getElementById("amigo").value.trim();
   if (amigo === "") {
     alert("Debes ingresar un nombre");
     return;
@@ -12,18 +12,39 @@ function agregarAmigo() {
   }
   document.getElementById("amigo").value = "";
   console.log(nombres);
+
+  actualizarLista();
 }
 
 //funcion actualizar lista de amigos
 
 function actualizarLista() {
-  let lista = document.getElementById("lista");
+  let lista = document.getElementById("listaAmigos");
   lista.innerHTML = "";
+
   nombres.forEach((amigo) => {
-    let li = document.createElement("li");
-    li.textContent = amigo;
-    lista.appendChild(li);
+      let li = document.createElement("li");
+      li.textContent = amigo;
+      lista.appendChild(li);
   });
 }
 
 //funcion para sortear a los amigos
+
+function sortearAmigo() {
+  if (nombres.length === 0) {
+    alert("Debes ingresar al menos un amigo para sortear.");
+    return;
+  }
+
+  let indiceAleatorio = Math.floor(Math.random() * nombres.length);
+
+  // Obtener el nombre sorteado
+  let amigoSorteado = nombres[indiceAleatorio];
+
+  // Mostrar el resultado en el HTML
+  let resultado = document.getElementById("amigoSorteado");
+  resultado.innerHTML = `El amigo secreto sorteado es: <strong>${amigoSorteado}</strong>`;
+  resultado.style.display = "block";
+}
+
